3fa8378a8e544cd62bb6528eec4fa9caf63b4f82
Brett Chapin <brett.chapin89@gmail.com>
2021-12-05 17:06:22 -0500

    Custom module to handle special input types from the user.

--- util/mielib/custominput.py
+++ util/mielib/custominput.py
@@ -1,6 +1,5 @@
 from .responseoption import ResponseOption, option, optionList
 
-
 def bool_input(output, default=None, abrv=True):
     '''A function to get a user's response to a question that has a yes or no
     (True or False) answer.
@@ -54,9 +53,84 @@ def choice_input(output, options, default=None, abrv=True):
         user_input = input(message)
         possible_option = option(options, user_input)
 
-        if user_input is "" and default.response is not None:
+        if user_input == "" and default.response is not None:
             return default.response
         elif possible_option is None:
             print("I'm sorry, I didn't understand that input.")
         else:
             return possible_option.response
+
+def range_input(output, lower, upper, default=None):
+    ''''''
+    message = "{} [{}-{}] ".format(output, lower, upper)
+    user_input = None
+    valid_input = False
+
+    while (not valid_input):
+        use_default = False
+        try:
+            user_input = input(message)
+
+            if user_input == "":
+                use_default = True
+
+            user_input = int(user_input)
+        except:
+            pass
+
+        if user_input in range(lower, upper + 1):
+            return user_input
+        elif use_default and default is not None:
+            return default
+        else:
+            print("I'm sorry, I didn't understand that input.")
+
+def regex_input(output, regex, default=None):
+    split_regex = regex.split(" ")
+    message = "{} [{}] ".format(output, regex)
+    valid_input = False
+    valid_num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    valid_alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", 
+    "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    valid_alpha_num = valid_num + valid_alpha
+
+    while (not valid_input):
+        user_input = str(input(message))
+        user_input_split = user_input.split(" ")
+        valid_format = True
+
+        if user_input == "" and default is not None:
+            return default
+        elif len(split_regex) is len(user_input_split):
+            for index in range(0, len(split_regex)):
+                item = split_regex[index]
+                check = user_input_split[index]
+            
+                if "/" in item:
+                    options = item.split("/")
+                    if check not in options:
+                        valid_format = False
+                        break
+                else:
+                    if len(item) is not len(check):
+                        valid_format = False
+                        break
+                    else:
+                        for inner_index in range(0, len(item)):
+                            if item[inner_index] == "#":
+                                if check[inner_index] not in valid_num:
+                                    valid_format = False
+                                    break
+                            elif item[inner_index] == "$":
+                                if check[inner_index] not in valid_alpha:
+                                    valid_format = False
+                                    break
+                            elif item[inner_index] == "*":
+                                if check[inner_index] not in valid_alpha_num:
+                                    valid_format = False
+                                    break
+            else:
+                if valid_format:
+                    return user_input
+
+        print("I'm sorry, I didn't understand that input.")
