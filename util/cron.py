00405fcd8144b4464eb39a74a18f18117a2158dc
Brett Chapin <brett.chapin89@gmail.com>
2021-12-12 12:37:15 -0500

    Updated the createRecurringJob() method

--- util/cron.py
+++ util/cron.py
@@ -8,15 +8,17 @@ class CronScheduler:
     # TODO: Add some functionality to convert a more human readable "date/time"
     #       input into an usable crantab time
 
-    def createRecurringJob(self, time, file, comment):
+    def createRecurringJob(self, time, command, comment):
         for job in self.cron:
             if job.comment == comment:
+                self.cron.remove(job)
+                new_job = self.cron.new(command=command, comment=comment)
+                new_job.setall(time)
+                self.cron.write()
                 break
         else:
-            command = 'sudo python /home/pi/minePi/cron/{} > /home/pi/minePi/logs.txt'.format(file)
             this_job = self.cron.new(command=command, comment=comment)
             this_job.setall(time)
-            print("Creating '{}' cron job...".format(comment))
             self.cron.write()
 
     def removeJob(self, comment):
